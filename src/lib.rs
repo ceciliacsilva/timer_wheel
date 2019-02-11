00ea42946e2cb697fd47a72c7dfb92a0e9764648
Cecília Carneiro <cecilia.carneiroesilva@gmail.com>
2019-02-11 09:44:49 -0200

    lib - WheelTimer -> TimerWheel; Cargo - version 0.2.0.

--- src/lib.rs
+++ src/lib.rs
@@ -10,15 +10,15 @@ use heapless::Vec;
 use heapless::consts::{U8, U4};
 
 /// Fixed ring size
-pub struct WheelTimer<T> {
+pub struct TimerWheel<T> {
     max_interval: usize,
     current_tick: usize,
     size: usize,
     ring: Vec<Vec<T, U4>, U8>,
 }
 
-impl<T> WheelTimer<T> {
-    pub fn new() -> WheelTimer<T>
+impl<T> TimerWheel<T> {
+    pub fn new() -> TimerWheel<T>
     {
         let mut ring: Vec<_, U8> = Vec::new();
         let max_interval = 8;
@@ -28,7 +28,7 @@ impl<T> WheelTimer<T> {
             let _ = ring.push(each_tick);
         }
 
-        WheelTimer{
+        TimerWheel{
             max_interval,
             current_tick: 0,
             ring,
@@ -67,9 +67,9 @@ extern crate rtfm;
 mod tests {
     #[test]
     fn one_schedule_one_tick() {
-        use crate::WheelTimer;
+        use crate::TimerWheel;
 
-        let mut wt = WheelTimer::new();
+        let mut wt = TimerWheel::new();
         wt.schedule(0, 1).unwrap();
         let to_run = wt.tick();
         assert_eq!(to_run[0], 1);
@@ -77,9 +77,9 @@ mod tests {
 
     #[test]
     fn schedule_lambda() {
-        use crate::WheelTimer;
+        use crate::TimerWheel;
 
-        let mut wt = WheelTimer::new();
+        let mut wt = TimerWheel::new();
         let f = || { 2+2 };
         let _ = wt.schedule(0, f);
         assert_eq!(wt.tick()[0](), 4);
@@ -87,9 +87,9 @@ mod tests {
 
     #[test]
     fn on_rtfm() {
-        use crate::WheelTimer;
+        use crate::TimerWheel;
 
-        let mut wt = WheelTimer::new();
+        let mut wt = TimerWheel::new();
         let _ = wt.schedule(0, rtfm::export::run(to_call));
 
         wt.tick()[0];
@@ -102,9 +102,9 @@ mod tests {
 
     #[test]
     fn tick_multiples() {
-        use crate::WheelTimer;
+        use crate::TimerWheel;
 
-        let mut wt = WheelTimer::new();
+        let mut wt = TimerWheel::new();
 
         wt.schedule(0, 1).unwrap();
         wt.schedule(3, 2).unwrap();
@@ -125,9 +125,9 @@ mod tests {
 
     #[test]
     fn clean_schedule_tick() {
-        use crate::WheelTimer;
+        use crate::TimerWheel;
 
-        let mut wt = WheelTimer::new();
+        let mut wt = TimerWheel::new();
 
         wt.schedule(0, 1).unwrap();
         wt.schedule(0, 2).unwrap();
@@ -142,9 +142,9 @@ mod tests {
 
     #[test]
     fn tick_pop() {
-        use crate::WheelTimer;
+        use crate::TimerWheel;
 
-        let mut wt = WheelTimer::new();
+        let mut wt = TimerWheel::new();
 
         wt.schedule(0, 1).unwrap();
         wt.schedule(0, 2).unwrap();
